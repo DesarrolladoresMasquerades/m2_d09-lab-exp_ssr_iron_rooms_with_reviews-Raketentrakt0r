4c2d918c37e011f1eae5a70f4c8cac3c25971114
Luis Moncada <raketentrakt0r@gmail.com>
2022-02-17 13:12:42 +0100

    first-commit

---
+++ rooms-app/models/User.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      password: String,
+      fullName:String,
+      slackID:String,
+      googleID:String,
+    },
+    
+  },
+
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
